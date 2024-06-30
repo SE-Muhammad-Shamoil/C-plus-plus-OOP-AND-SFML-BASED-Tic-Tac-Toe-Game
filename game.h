dfc5d8cbfa4b9b1c43127f354cd41d83ec0765f6
soft-eng-m-shamoil <shamoil004@gmail.com>
2024-06-30 22:13:58 +0500

    Added Game Header File

---
+++ game.h
@@ -0,0 +1,22 @@
+#include <SFMl/Graphics.hpp>
+#include <sfml/Audio.hpp>
+#include <iostream>
+using namespace std;
+
+class game
+{
+private:
+    sf::RenderWindow window;
+    sf::RectangleShape Background;
+    sf::Texture Bg_tex;
+public:
+    game()
+    {
+        
+    }
+
+    void start()
+    {
+        
+    }
+};
