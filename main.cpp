91cec73df2d738e8c307ac2af97489fc2e5bd6e0
soft-eng-m-shamoil <shamoil004@gmail.com>
2024-06-30 22:13:28 +0500

    Project Restarted

--- main.cpp
+++ main.cpp
@@ -1,40 +1,9 @@
 #include <iostream>
-#include <SFML/graphics.hpp>
-#include <vector>
+#include "game.h"
 using namespace std;
-
-class Move //(Tick/cross)
-{
-private:
-    sf::RectangleShape shape;
-
-public:
-    Move(sf::Vector2f Pos, sf::Texture txt)
-    {
-        shape.setTexture(&txt);
-        shape.setPosition(Pos);
-        shape.setSize(sf::Vector2f(50, 50));
-    }
-    void Draw(sf::RenderWindow &win)
-    {
-        win.draw(shape);
-    }
-};
-
-class Game
-{
-    sf::Window window;
-    sf::RectangleShape background;
-    vector<Move> moves;
-};
-
-class Menu
-{
-    private:
-    Game game;
-};
 
 int main()
 {
-    cout << "Hello";
+    game g;
+    g.start();
 }
